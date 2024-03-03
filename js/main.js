ee70c401f71f2e00db0322d8a74e17f460f0bb6f
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-03-03 12:31:05 -0300

    style scroll indicator

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
+
+abrir.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+})
+
+const enlaces = document.querySelectorAll(".nav-list a");
+
+enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+});
+
+
+
 const cursorDot = document.querySelector([".cursor-dot"]);
 const cursorOutline = document.querySelector([".cursor-outline"]);
 
@@ -30,21 +49,6 @@ window.addEventListener("mousemove", function (e)  {
   
 });
 
-abrir.addEventListener("click", ()=>{
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", ()=>{
-    nav.classList.remove("visible");
-})
-
-const enlaces = document.querySelectorAll(".nav-list a");
-
-enlaces.forEach((enlace) => {
-    enlace.addEventListener("click", () => {
-        nav.classList.remove("visible");
-    });
-});
 
 
 
