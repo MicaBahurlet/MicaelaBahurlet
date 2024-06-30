524f3c86c420d5cac8ae496836537f2cf9a6a0a6
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-06-30 19:39:25 -0300

    add img and certificates to formation

--- js/main.js
+++ js/main.js
@@ -21,34 +21,34 @@ enlaces.forEach((enlace) => {
 
 
 
-const cursorDot = document.querySelector([".cursor-dot"]);
-const cursorOutline = document.querySelector([".cursor-outline"]);
+// const cursorDot = document.querySelector([".cursor-dot"]);
+// const cursorOutline = document.querySelector([".cursor-outline"]);
 
-window.addEventListener("mousemove", function (e)  {
+// window.addEventListener("mousemove", function (e)  {
 
-    const posX = e.clientX;
-    const posY = e.clientY;
+//     const posX = e.clientX;
+//     const posY = e.clientY;
 
-    cursorDot.style.left = `${ posX }px`;
-    cursorDot.style.top = `${ posY }px`;
+//     cursorDot.style.left = `${ posX }px`;
+//     cursorDot.style.top = `${ posY }px`;
 
-    // cursorOutline.style.left = `${posX}px`;
-    // cursorOutline.style.top = `${posY}px`;
+//     // cursorOutline.style.left = `${posX}px`;
+//     // cursorOutline.style.top = `${posY}px`;
 
-    cursorOutline.animate({
+//     cursorOutline.animate({
         
-            left: `${posX}px`,
-            top: `${posY}px`
-        }, {
-            duration: 250,
-            fill: "forwards"
+//             left: `${posX}px`,
+//             top: `${posY}px`
+//         }, {
+//             duration: 250,
+//             fill: "forwards"
             
-        }
+//         }
 
-    );
+//     );
 
   
-});
+// });
 
 
 
